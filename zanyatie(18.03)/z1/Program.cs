dd8b25e4c2033f40c8906e25d284ae175803561d
ncolez <nicolebuilova@gmail.com>
2026-03-23 14:44:25 +0300

    Задания 18.03 классы в .NET

---
+++ zanyatie(18.03)/z1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace z1
+{
+    class A
+    {
+        public int a;
+        public int b;
+
+        public A(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int Multiply()
+        {
+            return a * b;
+        }
+
+        public double Expression()
+        {
+            return Math.Sqrt(b) / (2 * a);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            A obj = new A(a, b);
+
+            Console.WriteLine("Произведение a и b: " + obj.Multiply());
+            Console.WriteLine("Значение выражения √b / (2a): " + obj.Expression());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ zanyatie(18.03)/z2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace z2
+{
+    class Person
+    {
+        public string Name;
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+    }
+
+    static class ArrayUtils
+    {
+        public static void SortByName(Person[] persons)
+        {
+            for (int i = 0; i < persons.Length - 1; i++)
+            {
+                for (int j = i + 1; j < persons.Length; j++)
+                {
+                    if (string.Compare(persons[i].Name, persons[j].Name) > 0)
+                    {
+                        Person temp = persons[i];
+                        persons[i] = persons[j];
+                        persons[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имена: ");
+            string? input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Ошибка: имена не введены.");
+                Console.ReadLine();
+                return;
+            }
+
+            string[] names = input.Split(',');
+
+            Person[] persons = new Person[names.Length];
+            for (int i = 0; i < names.Length; i++)
+            {
+                string name = names[i].Trim();
+                persons[i] = new Person(name);
+            }
+
+            ArrayUtils.SortByName(persons);
+
+            Console.WriteLine("\nПосле сортировки:");
+            for (int i = 0; i < persons.Length; i++)
+            {
+                Console.WriteLine(persons[i].Name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
